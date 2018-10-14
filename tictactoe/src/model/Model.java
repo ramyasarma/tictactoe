95fadca62ecddbdca2b5d6cef6e20ff6ee914df3
Ramya Sarma <rsarma92@gmail.com>
2018-10-14 17:08:48 -0400

    Completed Model class to reflect changes from view and controller

--- tictactoe/src/model/Model.java
+++ tictactoe/src/model/Model.java
@@ -1,68 +1,91 @@
 package model;
- public class Model {
+
+import view.*;
+public class Model {
+	private View v;
 	private int player_id;
 	private int count_moves;
 	private char[][] grid;
 	private String message;
-	
-	// default constructor
 	public Model() {
 		this.grid = new char[3][3];
 		this.count_moves = 9;
+		this.player_id = 1;
 	}
-    
-	//setters and getters
+	public void referenceView(View v) {
+		this.v = v;
+	}
 	public int get_player_id() {
 		return player_id;
 	}
- 	public void set_player_id(int player_id) {
+
+	public void set_player_id(int player_id) {
 		this.player_id = player_id;
 	}
- 	public int get_count_moves() {
+
+	public int get_count_moves() {
 		return count_moves;
 	}
- 	public void set_count_moves(int count_moves) {
+
+	public void set_moves_count(int count_moves) {
 		this.count_moves = count_moves;
 	}
- 	public char[][] get_grid() {
+
+	public char[][] get_grid() {
 		return grid;
 	}
- 	public void set_grid(char[][] grid) {
-		this.grid = grid;
+
+	public void set_grid(char[][] board) {
+		this.grid = board;
 	}
- 	public String get_message() {
+
+	public String get_message() {
 		return message;
 	}
- 	public void set_message(String message) {
+
+	public void set_message(String message) {
 		this.message = message;
 	}
- 	// function to update the board
-	public void playMove(int x, int y, int player_id) {
+	public void playMove(int x, int y) {
+		
 		if(get_count_moves() > 0){
-			
-			if(player_id%2 != 0)
+			if(player_id%2 != 0) 
 				grid[x][y] = 'X';
-			else
+			else 
 				grid[x][y] = 'O';
-			
-			set_count_moves(--count_moves);
-			
-			if(isWinner(x, y, player_id))
-				set_message("Player" + player_id + "is Winner!");
-			if(get_count_moves()==0)
+			set_moves_count(--count_moves);
+			if(isWinner(x, y)) {
+				set_message("Player " + player_id + " is Winner!");
+				v.isWinner(x, y, grid[x][y], get_message());
+			}
+			else if(get_count_moves()==0) {
 				set_message("No Winner! Game ended in a Tie");
+				v.isWinner(x, y, grid[x][y], get_message());
+			}
+			else {
+				if(player_id%2 != 0) {
+					set_player_id(2);
+					set_message("'O':  Player " +get_player_id());
+				}
+				else {
+					set_player_id(1);
+					set_message("'X':  Player " +get_player_id());
+
+				}
+				v.update(x, y, grid[x][y], get_message());
+			}
+			
 		}
 		
 	}
 	
-	// function to check if there is a winner
-	public boolean isWinner(int x, int y, int player_id) {
+	public boolean isWinner(int x, int y) {
 		int countRow = 0;
 		int countCol = 0;
 		int countLDiag = 0;
 		int countRDiag = 0;
 		char symbol;
-		if(player_id %2 !=0)
+		if(get_player_id() %2 !=0)
 			symbol = 'X';
 		else
 			symbol = 'O';
@@ -83,6 +106,18 @@ package model;
 			return true;
 		
 		return false;
+	}
+	public void resetModel() {
+		count_moves = 9;
+		set_player_id(1);
+		set_message("");
+		for(int i=0; i<grid.length;i++) {
+			for(int j=0; j<grid.length;j++) {
+				grid[i][j] = '\0';
+			}
+		}
+		v.resetGame();
+		
 	}
 		
 }
