d54aefbbf51ca10bad5776ab15f85d59119a7f14
Ramya Sarma <rsarma92@gmail.com>
2018-10-14 16:20:33 -0400

    Got basic view class to work

--- tictactoe/src/view/View.java
+++ tictactoe/src/view/View.java
@@ -1,4 +1,5 @@
 package view;
+
 import controller.*;
 import adapter.*;
 import java.awt.*;
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 import javax.swing.*;
 
 public class View {
+	private Adapter adapter;
 	private JFrame gui;
     private JButton[][] blocks;
     private JButton reset;
@@ -20,7 +22,7 @@ public class View {
     		initialize();
     }
     public void setActionListener(Controller c) {
- 		this.adapter = new Adapter(c,this);
+		this.adapter = new Adapter(c,this);
 		for(int row = 0; row<3 ;row++) {
 	        for(int column = 0; column<3 ;column++) {
 	        		blocks[row][column].addActionListener(adapter);
@@ -28,16 +30,15 @@ public class View {
 		}
 	    reset.addActionListener(adapter);
     }
+    
     public void initialize () {
 		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    gui.setSize(new Dimension(500, 350));
 	    gui.setResizable(true);
-	    
 	    JPanel gamePanel = new JPanel(new FlowLayout());
 	    JPanel game = new JPanel(new GridLayout(3,3));
 	    gamePanel.add(game, BorderLayout.CENTER);
 	    JPanel options = new JPanel(new FlowLayout());
-	    options.add(reset);
 	    JPanel messages = new JPanel(new FlowLayout());
 	    messages.setBackground(Color.white);
 	    gui.add(gamePanel, BorderLayout.NORTH);
@@ -59,10 +60,32 @@ public class View {
 
     }
     public ArrayList<Integer> getPosition(ActionEvent e) {
-    }
-    public void update(int row, int column, char symbol, String message) {
-    }
-    public void isWinner(int row, int column, char symbol, String message) {
-    }
+    	ArrayList<Integer> position = new ArrayList<Integer>();
+    	for(int row = 0; row<3 ;row++) {
+	        for(int column = 0; column<3 ;column++) {
+	        		if(e.getSource() == blocks[row][column]) {
+	        			position.add(row);
+	        			position.add(column);
+	        		}
+	        }
+    		}
+    		return position;
     }
 
+    public void isWinner(int row, int column, char symbol, String message) {
+		blocks[row][column].setText(Character.toString(symbol));
+		blocks[row][column].setEnabled(false);
+		for(int i = 0; i<3 ;i++) {
+	        for(int j = 0; j<3 ;j++) {
+	        	blocks[i][j].setEnabled(false);
+	        }
+		}
+		playerturn.setText(message);
+
+    }
+    
+    public String get_button_text(int i, int j) {
+    		return blocks[i][j].getText();
+    }
+
+}
